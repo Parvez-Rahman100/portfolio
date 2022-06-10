aaac4679ec5a71c1ce953a7dba44edde79fc6963
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-06-10 22:57:16 +0600

    resume download option added

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import resume from '../../Assests/Resume-Parvez-Miah.pdf'
 
 const AboutMe = () => {
     return (
@@ -10,7 +11,7 @@ const AboutMe = () => {
       <h1 className="text-5xl font-bold">Hi , I am <span className=' text-green-600'>Parvez Miah</span> </h1>
       <h2 className='text-xl font-bold'>I am a <span className=' text-green-600'>Front-end Web Developer</span> </h2>
       <p className="py-6">I design and develop services for customers specializing creating stylish, modern websites, web services and online stores. My passion is to design digital user experiences through meaningful interactions. Check out my Portfolio.</p>
-      <a className='btn btn-primary' href="../../Assests/Resume-Parvez-Miah.pdf">GET RESUME</a>
+      <a className='btn btn-primary' href={resume} download>GET RESUME</a>
     </div>
   </div>
 </div>
