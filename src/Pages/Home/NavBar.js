0c4c34c98cd30c68c12558e9e2855d588e6b7a28
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-06-10 23:30:57 +0600

    final update for today

--- src/Pages/Home/NavBar.js
+++ src/Pages/Home/NavBar.js
@@ -10,22 +10,20 @@ const NavBar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><Link to='#about'>About Me</Link></li>
-        <li><Link to='#portfolio'>Portfolio</Link></li>
-        <li><Link to='#projects'>Projects</Link></li>
-        <li><Link to='/blogs'>Blogs</Link></li>
-        <li><Link to='#contact'>Contact Me</Link></li>
+        <li><Link href="home#about" to='#about'>About Me</Link></li>
+        <li><Link href="home#projects" to='#projects'>Projects</Link></li>
+        <li><Link  to='/blogs'>Blogs</Link></li>
+        <li><Link href="home#contact" to='#contact'>Contact Me</Link></li>
       </ul>
     </div>
-    <Link to='/' className="btn btn-ghost normal-case text-xl">Home</Link>
+    <Link to='/home' className="btn btn-ghost normal-case text-xl">Home</Link>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal p-0">
-      <li><Link to='#about'>About Me</Link></li>
-      <li><Link to='#portfolio'>Portfolio</Link></li>
-      <li><Link to='#projects'>Projects</Link></li>
+      <li><Link href="home#about" to='#about'>About Me</Link></li>
+      <li><Link href="home#projects" to='#projects'>Projects</Link></li>
       <li><Link to='/blogs'>Blogs</Link></li>
-      <li><Link to='#contact'>Contact Me</Link></li>
+      <li><Link href="home#contact" to='#contact'>Contact Me</Link></li>
     </ul>
   </div>
 </div>
